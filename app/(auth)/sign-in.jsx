9c343749b8cd2382820ddea8d70b2cd9a49ea846
Lukass9 <l_pisarek@o2.pl>
2024-09-14 08:04:25 +0200

    add auth screens

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,70 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import { images } from "../../constants";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
+
+  return (
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
+
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+            Login in to Aora
+          </Text>
+
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyle={"mt-7"}
+            keyboardType='email-address'
+          />
+
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyle={"mt-7"}
+          />
+
+          <CustomButton
+            title='Sign In'
+            handlePress={submit}
+            containerStyle='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link
+              href='/sign-up'
+              className='text-lg font-psemibold text-secondary'>
+              Sign up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
